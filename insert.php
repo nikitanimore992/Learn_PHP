ab825c644dcdf58b1146caaee4907331e2af500f
nikita nimore <992nikitanimore@gmail.com>
2024-08-23 00:05:30 +0530

    remark

--- insert.php
+++ insert.php
@@ -8,29 +8,24 @@
    <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
+  <center>
 <div class="container">
 
 <?php
 require "include/TopNavbar.php"
 ?>
  
- <div class="container">
-  
-  <?php 
-   require "include/topmenu.php";
-  ?>
- 
 
 <div id="mydata">
   <h1>
-    This is my insetr data page 
+    This is my insert data page 
   </h1>
 </div>
 
 </div>
 
 
-
+</center>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script> 
 </body>
 </html>
