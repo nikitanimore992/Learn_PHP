14b71bff8041e1ecf6dce8deff5b01bd893ae96c
nikita nimore <992nikitanimore@gmail.com>
2024-08-26 00:08:25 +0530

    set my insert page

--- insert.php
+++ insert.php
@@ -18,7 +18,6 @@ if (isset($_POST["submit"]))
    <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
-  <center>
 <div class="container">
 
 <?php
@@ -27,9 +26,12 @@ require "include/TopNavbar.php"
  
 
 <div id="mydata">
-  <h1>
+ 
+<center>
+<h1>
     This is my insert data page 
   </h1>
+</center>
   <?php
  if (isset($_POST["submit"]))
  {
@@ -52,7 +54,7 @@ require "include/TopNavbar.php"
 <form method="post" action="insert.php">
   <div class="mb-3">
     <label for="exampleInputEmail1" class="form-label">Enter employee id</label>
-    <input type="text" class="form-control" name="rno" aria-describedby="emailHelp">
+    <input type="text" class="form-control" name="eid" aria-describedby="emailHelp">
   </div>
   <div class="mb-3">
     <label for="exampleInputEmail1" class="form-label">Enter employee name</label>
@@ -60,11 +62,11 @@ require "include/TopNavbar.php"
   </div>
   <div class="mb-3">
     <label for="exampleInputEmail1" class="form-label">Enter employee department</label>
-    <input type="text" class="form-control" name="ct" aria-describedby="emailHelp">
+    <input type="text" class="form-control" name="dept" aria-describedby="emailHelp">
   </div>
   <div class="mb-3">
     <label for="exampleInputEmail1" class="form-label">Enter employee salary</label>
-    <input type="text" class="form-control" name="fs" aria-describedby="emailHelp">
+    <input type="text" class="form-control" name="sl" aria-describedby="emailHelp">
   </div>
   <button type="submit" name="submit" class="btn btn-primary">Submit</button>
 </form>
@@ -74,8 +76,6 @@ require "include/TopNavbar.php"
 
 </div>
 
-
-</center>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script> 
 </body>
 </html>
